5dc58fda9f1825bee19bf13bdd0a4f19bd735a8f
rooky-one <br-ian@hotmail.co.uk>
2023-07-27 20:57:50 +0100

    Lesson 9

--- hello.py
+++ hello.py
@@ -37,4 +37,35 @@ print('')
 for spreadsheet_cells in column_b:
     print(spreadsheet_cells.value)  # Print tuple contents as list
 
+print('')  # Add blank line
+
+# Lesson 9
+
+# Grab a Range
+cell_range = ws['A2':'A11']  # Just names exclude the column name
+print(range)  # See note...
+# Note the first 2 characters are (( this indicates a tuples in a tuple
+# This means to extract the cell data we need to loop through at least twice
+
+# Create one loop
+for cells_in_range in cell_range:
+    print(cells_in_range)  # Prints the cell ID not data held within
+
+print('')  # Add blank line
+
+# Create two loops
+for cells_in_range in cell_range:  # Set range
+    for x in cells_in_range:  # Loop through entire range
+        print(x.value)  # .value pulls out cell data
+
+print('')  # Add blank line
+
+# Grab a 2 Column in Range
+cell_range = ws['A2':'B11']  # Exclude the column names
+
+# Create two loops (gives column A then Column B in list)
+for cells_in_range in cell_range:  # Set range
+    for x in cells_in_range:  # Loop through entire range
+        print(x.value)  # .value pulls out cell data
+
 print(f'\n{"<End of hello.py>"}')  # Add newline to start line
