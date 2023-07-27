af270f13ba2eeb78666f37dee9337d9ade676d6a
rooky-one <br-ian@hotmail.co.uk>
2023-07-27 20:21:55 +0100

    Lesson 8

--- hello.py
+++ hello.py
@@ -12,12 +12,29 @@ wb = load_workbook('hello.xlsx')
 ws = wb.active
 
 # Use a variable to hold spreadsheet call value
-name = (ws['A2'].value)
-colour = (ws['B2'].value)
+name = ws['A2'].value
+colour = ws['B2'].value
 
 # Print something from our spreadsheet
 print(ws['A2'].value)
 print(ws['B2'].value)
 print(f'{name}: {colour}\n')
 
-print("<End of hello.py>")
+# Grab a whole column
+column_a = ws['A']  # Returns immutable tuple
+column_b = ws['B']  # Returns immutable tuple
+
+print(f'{column_a} \n')  # Prints the immutable tuple list
+print(f'{column_b} \n')  # Prints the immutable tuple list
+
+# Loop through the tuple
+for spreadsheet_cells in column_a:
+    print(spreadsheet_cells.value)  # Print tuple contents as list
+
+print('')
+
+# Loop through the tuple
+for spreadsheet_cells in column_b:
+    print(spreadsheet_cells.value)  # Print tuple contents as list
+
+print(f'\n{"<End of hello.py>"}')  # Add newline to start line
