98f6eb1650acc9b91092465d883f4e8509cdbc0d
rooky-one <br-ian@hotmail.co.uk>
2023-07-28 18:47:33 +0100

    Lesson 13 Change cells 02nd

--- tutorial_02.py
+++ tutorial_02.py
@@ -2,25 +2,43 @@ from openpyxl.workbook import Workbook
 from openpyxl import load_workbook
 #  openpyxl ia a Python library to read/write Excel 2010 xlsx/xlsm/xltx/xltm files
 
-print(f"<Start of tutorial_01.py>\n")
+print(f"<Start of tutorial_02.py>\n")
 # Create workbook object
 # wb = Workbook()
 
 # load existing spreadsheet
-wb = load_workbook('tutorial_01.xlsx')
+wb = load_workbook('tutorial_02.xlsx')
 
 # Create an active worksheet
 ws = wb.active
 
-# Lesson 12 - Change Cells and Save Spreadsheet
-print(f"Lesson 12 - Change Cells and Save Spreadsheet\n")
+# Lesson 12 - Change Cells and Save Spreadsheet (1st Method)
+print(f"Lesson 12 - Change Cells and Save Spreadsheet (1st Method)\n")
 
 # Change a cell
 
 ws["A11"] = "Judas"
 
 # Save the spreadsheet (two options)
-# wb.save('C:\code\python-excel\tutorial.xlsx')  # Absolute path
+# wb.save('C:\code\python-excel\tutorial_02.xlsx')  # Absolute path
 wb.save('tutorial_02.xlsx')  # Relative save, note save under new file name
 
 print("File was saved...")
+print("")
+
+# Lesson 13 - Add Cells To A Spreadsheet (Second Method)
+print(f"Lesson 13 - Add Cells To A Spreadsheet (Second Method)\n")
+
+# Change a many cells
+
+starting_row = 12
+ws.cell(row=11, column=1).value = "Bartholomew"
+ws.cell(row=11, column=2).value = "Black"
+
+wb.save('tutorial_02.xlsx')
+
+print("")
+print("File was saved...")
+print("")
+
+print(f'\n{"<End of tutorial_02.py>"}')  # Add newline to start line
